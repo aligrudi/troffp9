33b8bae3f0e53c2b16c263cf52e99e124644ccf2
Ali Gholami Rudi <ali@rudi.ir>
2011-07-29 00:27:11 +0430

    tr2ps: cleanup unused macros in path.h

--- tr2ps/path.h
+++ tr2ps/path.h
@@ -9,15 +9,6 @@
 #endif
 
 #define DPOST		TBASE "/ps/prologues/dpost.ps"
-#define POSTBGI		TBASE "/ps/prologues/postbgi.ps"
-#define POSTDAISY	TBASE "/ps/prologues/postdaisy.ps"
-#define POSTDMD		TBASE "/ps/prologues/postdmd.ps"
-#define POSTMD		TBASE "/ps/prologues/postmd.ps"
-#define POSTPLOT	TBASE "/ps/prologues/postplot.ps"
-#define POSTPRINT	TBASE "/ps/prologues/postprint.ps"
-#define POSTNPRINT	TBASE "/ps/prologues/postnprint.ps"
-#define POSTTEK		TBASE "/ps/prologues/posttek.ps"
-#define POSTGIF		TBASE "/ps/prologues/postgif.ps"
 
 #define BASELINE	TBASE "/ps/prologues/baseline.ps"
 #define COLOR		TBASE "/ps/prologues/color.ps"
@@ -29,7 +20,4 @@
 #define ROUNDPAGE	TBASE "/ps/prologues/roundpage.ps"
 
 #define ENCODINGDIR	TBASE "/ps/prologues"
-#define HOSTDIR		TBASE "/ps/font"
 #define FONTDIR		TBASE "/font"
-#define POSTLIBDIR	TBASE "/ps/prologues"
-#define TEMPDIR		"/tmp"
