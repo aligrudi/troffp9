11a60c48981b8b5a5574f83918de6c0ae399fc31
Ali Gholami Rudi <ali@rudi.ir>
2011-07-31 18:22:02 +0430

    include troff data directory

---
+++ mkdev/mkdev.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# path of troff data directory
+TBASE="/root/tr/home"
+DEV="utf"
+
+DST=$TBASE/font/dev$DEV
+
+. $DST/shell.lib
+ALL=`AllTables`
+for f in $ALL
+do
+	echo $f
+	sh trofftable.sh -D$TBASE/ps/prologues/dpost.ps -L./trofftable.ps \
+			-F$TBASE/font -T$DEV $f >out.ps
+	ps2pdf out.ps >$DST/$f
+done
+
+rm out.ps out.pdf
