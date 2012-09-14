35838a48b535c58cc8cc8a64136e42f1b5dbf417
Ali Gholami Rudi <ali@rudi.ir>
2012-09-14 14:25:22 +0430

    tr2ps: move postscript prologues to home/ps

--- tr2ps/path.h
+++ tr2ps/path.h
@@ -8,16 +8,16 @@
 #define TBASE		"/troff/home"
 #endif
 
-#define DPOST		TBASE "/ps/prologues/dpost.ps"
+#define DPOST		TBASE "/ps/dpost.ps"
 
-#define BASELINE	TBASE "/ps/prologues/baseline.ps"
-#define COLOR		TBASE "/ps/prologues/color.ps"
-#define DRAW		TBASE "/ps/prologues/draw.ps"
-#define FORMFILE	TBASE "/ps/prologues/forms.ps"
-#define SHADEFILE	TBASE "/ps/prologues/shade.ps"
-#define KERNING		TBASE "/ps/prologues/kerning.ps"
-#define REQUESTFILE	TBASE "/ps/prologues/ps.requests"
-#define ROUNDPAGE	TBASE "/ps/prologues/roundpage.ps"
+#define BASELINE	TBASE "/ps/baseline.ps"
+#define COLOR		TBASE "/ps/color.ps"
+#define DRAW		TBASE "/ps/draw.ps"
+#define FORMFILE	TBASE "/ps/forms.ps"
+#define SHADEFILE	TBASE "/ps/shade.ps"
+#define KERNING		TBASE "/ps/kerning.ps"
+#define REQUESTFILE	TBASE "/ps/ps.requests"
+#define ROUNDPAGE	TBASE "/ps/roundpage.ps"
 
-#define ENCODINGDIR	TBASE "/ps/prologues"
+#define ENCODINGDIR	TBASE "/ps"
 #define FONTDIR		TBASE "/font"
