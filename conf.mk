dbad9a7632595b111f56a4193da0b151a2dd37f4
Ali Gholami Rudi <ali@rudi.ir>
2020-10-03 18:28:52 +0330

    keep only pic and tbl for neatroff

--- conf.mk
+++
@@ -1,2 +0,0 @@
-# the path of troff data folder; "home" directory in this package
-TBASE = "/root/c/troff/home"
--- home/tmac/tmac.m
+++
@@ -1,3 +0,0 @@
-'''\"	TMAC.M @(#)tmacs.src	1.7
-.if n .so \*(.P/mmn
-.if t .so \*(.P/mmt
